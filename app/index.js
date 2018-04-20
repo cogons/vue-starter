add8704c76d7b72695ee63fe3f34ca421f1d43f4
Cogons <cogons@qq.com>
2018-04-20 11:40:07 +0800

    first commit

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App'
+import router from 'vue-router'
+
+new Vue({
+    el: '#app',
+    router,
+    // store,
+    render: h => h(App),
+  });
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+//定义了一些文件夹的路径
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+module.exports = {
+    //项目的文件夹 可以直接用文件夹名称 默认会找index.js 也可以确定是哪个文件名字
+    entry: APP_PATH,
+    //输出的文件名 合并以后的js会命名为bundle.js
+    output: {
+        path: BUILD_PATH,
+        filename: 'bundle.js'
+    },
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true,
+    },
+    externals: {
+        vue: 'Vue',
+        'vue-router': 'VueRouter'
+    },
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+        //   'vue$': 'vue/dist/vue.esm.js',
+          '@': path.resolve(__dirname, 'app')
+        }
+      },
+    module: {
+        rules: [
+          {
+            test: /\.vue$/,
+            loaders: ['vue-loader']
+          }
+        ]
+      },
+    //添加我们的插件 会自动生成一个html文件
+    plugins: [
+        new HtmlwebpackPlugin({
+            title: 'Hello World app',
+            template: 'index.html'
+        })
+    ]
+};
